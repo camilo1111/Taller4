f31b8ce8c4779446c67d42e4244130e7d77e8f99
CAMILO <cdiaz29@uan.edu.co>
2020-05-11 13:00:36 -0500

    archivos de descripcion

--- app/src/main/java/com/uan/taller4/DetailActivity.kt
+++ app/src/main/java/com/uan/taller4/DetailActivity.kt
@@ -2,11 +2,55 @@ package com.uan.taller4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        setName()
+        setPhoto()
+        setDescription()
+
+    }
+
+    fun setName(){
+        val contentName = this.intent.extras
+        if (contentName != null){
+            val titleName= contentName.getString("name")
+            textViewName.setText(titleName)
+        }
+    }
+
+    fun setPhoto(){
+        val contentPhoto = this.intent.extras
+        if (contentPhoto != null){
+            val namePhoto =contentPhoto.getString("photo")
+            val idImage = resources.getIdentifier(namePhoto,"drawable", packageName)
+            imageViewPhoto.setImageResource(idImage)
+
+        }
+
+    }
+
+    fun setDescription(){
+        val contentDescription = this.intent.extras
+        var characterDescription = ""
+        if (contentDescription != null){
+            val nameDescription =contentDescription.getString("description")
+            val idDescription = resources.getIdentifier(nameDescription,"raw", packageName)
+
+            val scanner = Scanner(resources.openRawResource(idDescription))
+            while (scanner.hasNextLine()){
+                val lineText = scanner.nextLine()
+                characterDescription += lineText
+            }
+            textViewDescription.setText(characterDescription)
+            scanner.close()
+
+        }
+
     }
 }
--- app/src/main/java/com/uan/taller4/MainActivity.kt
+++ app/src/main/java/com/uan/taller4/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.uan.taller4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ImageBtnHomer.setOnClickListener {
+            val continueDetail = Intent (this, DetailActivity::class.java)
+            continueDetail.putExtra("name","Homer Simpson" )
+            continueDetail.putExtra("photo","homer" )
+            continueDetail.putExtra("description", "homer")
+            startActivity(continueDetail)
+        }
+
+        ImageBtnBart.setOnClickListener {
+            val continueDetail = Intent (this, DetailActivity::class.java)
+            continueDetail.putExtra("name","Bart Simpson" )
+            continueDetail.putExtra("photo","bart" )
+            continueDetail.putExtra("description", "bart")
+            startActivity(continueDetail)
+        }
+
+        ImageBtnMarge.setOnClickListener {
+                val continueDetail = Intent (this, DetailActivity::class.java)
+                continueDetail.putExtra("name","Marge Simpson" )
+                continueDetail.putExtra("photo","marge" )
+                continueDetail.putExtra("description", "marge")
+                startActivity(continueDetail)
+        }
+
+        ImageBtnLisa.setOnClickListener {
+            val continueDetail = Intent (this, DetailActivity::class.java)
+            continueDetail.putExtra("name","Lisa Simpson" )
+            continueDetail.putExtra("photo","lisa" )
+            continueDetail.putExtra("description", "lisa")
+            startActivity(continueDetail)
+        }
+
+        ImageBtnMaggie.setOnClickListener {
+            val continueDetail = Intent (this, DetailActivity::class.java)
+            continueDetail.putExtra("name","Maggie Simpson" )
+            continueDetail.putExtra("photo","maggie" )
+            continueDetail.putExtra("description", "maggie")
+            startActivity(continueDetail)
+        }
+
+        ImageBtnAbraham.setOnClickListener {
+            val continueDetail = Intent (this, DetailActivity::class.java)
+            continueDetail.putExtra("name","Abraham Simpson" )
+            continueDetail.putExtra("photo","abraham" )
+            continueDetail.putExtra("description", "abraham")
+            startActivity(continueDetail)
+        }
     }
 }
